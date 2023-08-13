a02fdc9d0f68c0ac7417c7b99f106770e24f8dcc
AbhishekTAILOR700 <85023018+AbhishekTAILOR700@users.noreply.github.com>
2023-08-13 16:12:59 +0530

    Update index.js
    added animation for the buttons

--- index.js
+++ index.js
@@ -1,68 +1,120 @@
 var noOfButtons = document.querySelectorAll(".drum").length;
+for(var i = 0; i<noOfButtons; i++){
 
-for(var i = 0; i< noOfButtons; i++){
-    
-    document.querySelectorAll("button")[i].addEventListener("click", function (){
-        // console.log(this.innerHTML);
-
-        var buttonInnerHTML = this.innerHTML;
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         
-        makeSound(buttonInnerHTML);
-       
+        // console.log(this.innerHTML);
+        // if(this === "w"){
+        //     document.querySelector("w").style.color = "green";
+        // }
+        // this.style.color = "green";
+        var buttonInnerHtml = this.innerHTML;
+
+        buttonAnimation(buttonInnerHtml);
+        switch (buttonInnerHtml) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+        
+            default:
+                break;
+        }
     });
-    
 }
 
+
+
 document.addEventListener("keydown", function (event){
-    makeSound(event.key);
-})
-
-
-function makeSound(key){
+    // alert("hello, I am under the water, Help me");
     
-    switch (key) {
+    console.log(event.key);
+    
+    buttonAnimation(event.key);
+
+    switch (event.key) {
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
             break;
-        
+
         case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");   
-            tom2.play(); 
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
             break;
 
         case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
             break;
 
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
             break;
 
         case "j":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
             break;
 
         case "k":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
-        
-        case "l":
-            var kick = new Audio("sounds/kick-bass.mp3");
-            kick.play();
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
             break;
 
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    
         default:
-            console.log(this.innerHTML);
             break;
     }
-}
-
-
-
+});
 // var audio = new Audio("sounds/tom-1.mp3");
-//         audio.play();
+// audio.play();
+
+
+
+// Button ANimation
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){
+        activeButton.classList.remove("pressed")
+        
+    }, 100);
+}
